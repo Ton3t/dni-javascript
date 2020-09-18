744fdd4112f5ae99038f008e2c458dac25e2e91f
Ton3t <elton3t@gmail.com>
2020-09-18 22:17:02 +0200

    Programa terminado

---
+++ app.js
@@ -0,0 +1,20 @@
+function calculaLetra() {
+    var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+    var num = parseInt(prompt("Ingresa DNI:"));
+
+    if(isNaN(num)) {
+        alert("Número erroneo");
+    }
+
+    else {
+        for(var i = 0; i < letras.length; i++) {
+            if(num % 23 == i) {
+                alert(num + letras[i]);
+            }
+        }
+    }
+}
+
+window.onload = function() {
+    this.calculaLetra();
+}
